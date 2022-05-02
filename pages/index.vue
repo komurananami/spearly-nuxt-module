ee21cc5b4dda6def94e52f6975c6a8f1ef0614b5
nanami <kom_nana5102@icloud.com>
2022-05-02 15:11:06 +0900

    :sparkles: add form

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,51 @@
         <AuthorIntro></AuthorIntro>
         <Expertise></Expertise>
         <TimeLine></TimeLine>
+
+        <spearly-form id="f-5CXBGn30HWINKdc9twRT">
+          <template v-slot="form">
+            <fieldset
+              v-for="field in form.fields"
+              :key="field.identifier"
+              class="f-fieldset"
+            >
+              <label :for="field.identifier">
+                {{ field.name }}
+                <i v-if="field.required">*</i>
+              </label>
+
+              <input
+                v-if="field.inputType === 'text'"
+                :id="field.identifier"
+                v-model="answers[field.identifier]"
+                :required="field.required"
+                type="text"
+                :placeholder="field.description"
+                class="f-input"
+              />
+              <textarea
+                v-else-if="field.inputType === 'text_area'"
+                :id="field.identifier"
+                v-model="answers[field.identifier]"
+                :required="field.required"
+                :placeholder="field.description"
+                class="f-input"
+              />
+            </fieldset>
+
+            <input
+              v-model="answers._spearly_gotcha"
+              type="text"
+              style="
+                position: absolute;
+                width: 1px;
+                height: 1px;
+                overflow: hidden;
+              "
+            />
+            <button @click="form.submit(answers)" class="f-btn">送信</button>
+          </template>
+        </spearly-form>
       </div>
     </div>
   </div>
@@ -19,6 +64,12 @@ export default {
   data() {
     return {
       siteMetaInfo: siteMetaInfo,
+      answers: {
+        name: "",
+        email: "",
+        form: "",
+        _spearly_gotcha: "",
+      },
     };
   },
   head: {
@@ -37,4 +88,23 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.f-input {
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  padding: 4px 8px;
+}
+
+.f-btn {
+  background: #000;
+  color: #fff;
+  padding: 4px 8px;
+  border-radius: 6px;
+}
+
+.f-fieldset {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+}
+</style>
